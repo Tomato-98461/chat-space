d81594b521edf9aa65d5109f3c3a367492feac31
Tomato-98461 <aq10.tomato@gmail.com>
2019-10-16 12:59:55 +0900

    add(User And Group Controller,View)

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,32 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @group = Group.find(:id)
+  end
+
+  def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids:[])
+  end
+end
