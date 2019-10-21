235e16a3d80349b0413998101c018c1c59d41453
Tomato-98461 <aq10.tomato@gmail.com>
2019-10-21 14:58:34 +0900

    create (app/javascripts/message.js)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,11 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path, notice: '送信完了'
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(params[:group_id])  }
+        format.json
+      end
+      # redirect_to group_messages_path, notice: '送信完了'
     else
       @messages = Message.where(group_id: params[:group_id])
       flash[:alert] = 'メッセージを入力してください。'
