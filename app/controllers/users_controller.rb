0f195eb80a71407e5ec8f3c97c12485629fb51a7
Tomato-98461 <aq10.tomato@gmail.com>
2019-10-29 15:24:19 +0900

    delete commentOut

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: params[:user_ids])
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: params[:user_ids]) #name: current_user.name
     respond_to do |format|
       format.html
       format.json
